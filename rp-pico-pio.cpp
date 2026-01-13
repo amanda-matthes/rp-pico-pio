8fe06d609e36a78c60e241530b4af65d081992ea
Amanda Matthes <amanda.matthes@gmail.com>
2026-01-13 22:51:12 +0000

    ultra basic pin toggle

--- rp-pico-pio.cpp
+++ rp-pico-pio.cpp
@@ -5,6 +5,7 @@
 #include "test.pio.h" // this is generated from test.pio and defines test_program and test_program_get_default_config - use pico_generate_pio_header in CMakeLists.txt
 
 #include "pin_toggle.pio.h"
+#include "pin_toggle_basic.pio.h"
 
 /**
  * PIO example: test
@@ -12,28 +13,61 @@
 void pio_test()
 {
     // pick a PIO block and a state machine within that PIO (Pico 2 has 3 PIO blocks with 4 state machines each)
-    PIO test_pio = pio0;
-    uint test_state_machine = 1;
-    uint test_pin = 2;
+    PIO pio_block = pio0;         // PIO block 0
+    uint state_machine_index = 1; // state machine 1
+    uint pin = 2;                 // GPIO pin 2
 
-    // add program to pio instruction memory (on the Pico 2, that can be 32 instructions per PIO)
-    uint test_memory_offset = pio_add_program(test_pio, &test_program);
-    pio_sm_config test_state_machine_config = test_program_get_default_config(test_memory_offset);
+    uint pin_toggle_frequency_Hz = 5;
 
-    // slow down clock
-    sm_config_set_clkdiv_int_frac8(&test_state_machine_config, 10, 0);
+    // add program to pio instruction memory
+    uint memory_offset = pio_add_program(pio_block, &pin_toggle_basic_program);
+    pio_sm_config state_machine_config = pin_toggle_basic_program_get_default_config(memory_offset);
 
-    // set the test pin as the output of the PIO state machine
-    pio_gpio_init(test_pio, test_pin);
-    pio_sm_set_consecutive_pindirs(test_pio, test_state_machine, test_pin, 1, true);
-    sm_config_set_set_pins(&test_state_machine_config, test_pin, 1);
+    // set the output pin of the PIO state machine
+    pio_gpio_init(pio_block, pin);
+    pio_sm_set_consecutive_pindirs(pio_block, state_machine_index, pin, 1, true);
+    sm_config_set_set_pins(&state_machine_config, pin, 1);
+
+    // slow down clock to adjust frequency
+    uint32_t clock_divider = SYS_CLK_HZ / (2 * pin_toggle_frequency_Hz);
+    sm_config_set_clkdiv_int_frac8(&state_machine_config, clock_divider, 0);
 
     // initialise the state machine
-    pio_sm_init(test_pio, test_state_machine, test_memory_offset, &test_state_machine_config);
-    pio_sm_set_enabled(test_pio, test_state_machine, true);
+    pio_sm_init(pio_block, state_machine_index, memory_offset, &state_machine_config);
+    pio_sm_set_enabled(pio_block, state_machine_index, true);
 
     // add something to the FIFO of the state machine
-    test_pio->txf[test_state_machine] = 0xAFAFAFAF;
+    pio_block->txf[state_machine_index] = 0xAFAFAFAF;
+}
+
+/**
+ * PIO example: Toggle GPIO pin by slowing down the state machine clock
+ */
+void pio_pin_toggle_basic()
+{
+    PIO pio_block = pio0;         // use PIO block 0
+    uint state_machine_index = 1; // use state machine 1
+    uint pin = 2;                 // use GPIO pin 2
+
+    uint pin_toggle_frequency_Hz = 500000; // 500 kHz
+
+    // add program to pio instruction memory
+    uint memory_offset = pio_add_program(pio_block, &pin_toggle_basic_program);
+    pio_sm_config state_machine_config = pin_toggle_basic_program_get_default_config(memory_offset);
+
+    // set the output pin of the PIO state machine
+    pio_gpio_init(pio_block, pin);
+    pio_sm_set_consecutive_pindirs(pio_block, state_machine_index, pin, 1, true);
+    sm_config_set_set_pins(&state_machine_config, pin, 1);
+
+    // slow down clock to adjust frequency
+    uint32_t required_state_machine_clock_Hz = 6 * pin_toggle_frequency_Hz; // 3 cycles high, 3 cycles low
+    float clock_divider = SYS_CLK_HZ / required_state_machine_clock_Hz;
+    sm_config_set_clkdiv(&state_machine_config, clock_divider);
+
+    // initialise the state machine
+    pio_sm_init(pio_block, state_machine_index, memory_offset, &state_machine_config);
+    pio_sm_set_enabled(pio_block, state_machine_index, true);
 }
 
 /**
@@ -41,9 +75,9 @@ void pio_test()
  */
 void pio_pin_toggle()
 {
-    PIO pin_toggle_pio = pio0;         // wee will use PIO block 0
-    uint pin_toggle_state_machine = 1; // we will use state machine 1
-    uint pin_toggle_pin = 2;           // we will toggle GPIO pin 2
+    PIO pin_toggle_pio = pio0;         // use PIO block 0
+    uint pin_toggle_state_machine = 1; // use state machine 1
+    uint pin_toggle_pin = 2;           // use GPIO pin 2
     uint pin_toggle_frequency_Hz = 1000000;
 
     // add program to pio instruction memory
@@ -94,7 +128,8 @@ int main()
 
     // PIO example - uncomment ONE SINGLE example at a time (they may conflict on using the same PIO block / state machine / GPIO pins)
     // pio_test();
-    pio_pin_toggle();
+    pio_pin_toggle_basic();
+    // pio_pin_toggle();
 
     // loop to keep going and for LED sanity check
     // (some examples may not reach this)
