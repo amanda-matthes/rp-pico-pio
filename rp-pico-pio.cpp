143b1d62c13a4b5758fa40ed206614ae4fbe358c
Amanda Matthes <amanda.matthes@gmail.com>
2026-01-14 01:13:31 +0000

    saved tx to GPIO example

--- rp-pico-pio.cpp
+++ rp-pico-pio.cpp
@@ -6,6 +6,7 @@
 
 #include "pin_toggle.pio.h"
 #include "pin_toggle_basic.pio.h"
+#include "move_data_TX_to_GPIO.pio.h"
 
 /**
  * PIO example: test
@@ -21,6 +22,52 @@ void pio_test()
     // add program to pio instruction memory
     uint memory_offset = pio_add_program(pio_block, &test_program);
     pio_sm_config state_machine_config = test_program_get_default_config(memory_offset);
+
+    // set the output pin of the PIO state machine
+    pio_gpio_init(pio_block, clock_pin);
+    pio_gpio_init(pio_block, data_pin);
+    pio_sm_set_consecutive_pindirs(pio_block, state_machine_index, clock_pin, 1, true);
+    pio_sm_set_consecutive_pindirs(pio_block, state_machine_index, data_pin, 1, true);
+    // here we are using the GPIO pin with the out and set instruction, so we need to set out pins accordingly
+    sm_config_set_out_pins(&state_machine_config, data_pin, 1);
+    sm_config_set_set_pins(&state_machine_config, data_pin, 1);
+    // we are using side set to drive the clock pin
+    sm_config_set_sideset_pins(&state_machine_config, clock_pin);
+
+    // slow down clock to adjust frequency
+    uint32_t required_state_machine_clock_Hz = 1000000; // 1 MHz
+    float clock_divider = SYS_CLK_HZ / required_state_machine_clock_Hz;
+    sm_config_set_clkdiv(&state_machine_config, clock_divider);
+
+    // initialise the state machine
+    pio_sm_init(pio_block, state_machine_index, memory_offset, &state_machine_config);
+    pio_sm_set_enabled(pio_block, state_machine_index, true);
+
+    uint8_t x1 = 0xAB; // example byte for shift register 1
+    uint8_t x2 = 0xCD; // example byte for shift register 2
+
+    // concatenate the two bytes into a single 32-bit word
+    uint32_t data_word = ((uint32_t)x1 << 24) | ((uint32_t)x2 << 16);
+
+    // add something to the FIFO of the state machine
+    while (true)
+    {
+        pio_block->txf[state_machine_index] = data_word;
+        sleep_us(200);
+    }
+}
+
+void pio_move_data_TX_to_GPIO()
+{
+    PIO pio_block = pio0;         // PIO block 0
+    uint state_machine_index = 1; // state machine 1
+
+    uint clock_pin = 2;
+    uint data_pin = 3;
+
+    // add program to pio instruction memory
+    uint memory_offset = pio_add_program(pio_block, &move_data_TX_to_GPIO_program);
+    pio_sm_config state_machine_config = move_data_TX_to_GPIO_program_get_default_config(memory_offset);
 
     // set the output pin of the PIO state machine
     pio_gpio_init(pio_block, clock_pin);
